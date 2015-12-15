59de14782c29d73fc0096f40d509ce40309e2155
Francisco Temudo <fthemudo@gmail.com>
2015-12-15 14:45:21 +0000

    Prevents double app setup

--- api/app.rb
+++ api/app.rb
@@ -2,9 +2,11 @@ module Animals
   module Api
     class App < Grape::API
       def self.setup_application!
+        return if @_configured
         require_dependencies
         error_formatter :json, Animals::Api::ErrorFormatter
         mount Animals::Api::V1::Root
+        @_configured = true
       end
       rescue_from Grape::Exceptions::ValidationErrors do |e|
         handler = lambda {|arg| error_response(arg)}
